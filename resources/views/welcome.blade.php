9a18c32ced9fe6c04ffdac3a494e9e1c48cb4144
Joseph Nderitu <josephtech61@gmail.com>
2021-10-30 09:06:51 +0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/css/app.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@500&family=Merienda&family=Work+Sans:wght@500&display=swap" rel="stylesheet">
+    <title>Tours and Travel</title>
+</head>
+<body>
+    <div id="MainPage">
+        <div id="wrapper">
+            <nav class="navbar main-navbar navbar-expand-lg navbar-light">
+                <a href="/" class="navbar-brand"> <img src="/storage/logo.png" class="mainlogo" alt=""> </a>                
+            </nav>
+        
+            <div class="hero-section">
+                <img src="/storage/frontend/lionsunset.jpg" alt="Safari Lion Image" width="100%" height="100%">
+
+                <div class="hero-text">
+                    <h4>Enjoy the solitude and solace of</h4>
+                    <h1>Africa Safaris</h1>  
+                    <hr>    
+                    
+                    <div class="btn-cta-holder">
+                        <button class="btn btn-md mt-5 btn-find-out-more">Explore</button>
+                        <div class="small-btn-arrow"></div>
+                    </div>                    
+                </div>
+            </div>
+
+            <div class="mid-nav">
+                <nav class="navbar navbar-expand-lg">
+                    <ul class="navbar-nav">
+                    <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
+                        <li class="nav-item"><a href="/" class="nav-link">About Us</a></li>
+                        <li class="nav-item"><a href="/" class="nav-link">Destinations</a></li>
+                        <li class="nav-item"><a href="/" class="nav-link">Honeymoon</a></li>
+                        <li class="nav-item"><a href="/" class="nav-link">Activities</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="mdi-top-info container-fluid p-0 m-0">
+                <div class="row">
+                    <div class="col-md-6 mid-img-holder p-0 m-0">
+                        <img src="/storage/frontend/cheetah.jpg" alt="Cheetah" width="100%" height="100%">
+                    </div>
+                    <div class="col-md-6 mid-img-holder p-0 m-0">
+                        <div class="row">
+                            <div class="col-md-6 mid-img-holder p-0 m-0">
+                                <img src="/storage/frontend/giraffe.jpg" alt="Giraffe" width="100%" height="100%">
+                            </div>
+                            <div class="col-md-6 mid-img-holder p-0 m-0">
+                                <img src="/storage/frontend/lion.jpg" alt="Giraffe" width="100%" height="100%">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6 mid-img-holder p-0 m-0">
+                        <div class="row">
+                            <div class="col-md-6 mid-img-holder p-0 m-0">
+                                <img src="/storage/frontend/giraffe.jpg" alt="Giraffe" width="100%" height="100%">
+                            </div>
+                            <div class="col-md-6 mid-img-holder p-0 m-0">
+                                <img src="/storage/frontend/wilderbeast.jpeg" alt="Giraffe" width="100%" height="100%">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6 mid-img-holder p-0 m-0">
+                        <img src="/storage/frontend/zebras.jpg" alt="Cheetah" width="100%" height="100%">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
