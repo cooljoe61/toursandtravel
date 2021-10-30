76c5cc81ffbed819f66cb005b46e29e0af771c9f
Joseph Nderitu <josephtech61@gmail.com>
2021-10-30 21:51:55 +0300

    Init tes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@500&family=Merienda&family=Work+Sans:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.9.55/css/materialdesignicons.min.css" integrity="sha512-vIgFb4o1CL8iMGoIF7cYiEVFrel13k/BkTGvs0hGfVnlbV6XjAA0M0oEHdWqGdAVRTDID3vIZPOHmKdrMAUChA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
     <title>Tours and Travel</title>
 </head>
 <body>
@@ -34,7 +36,7 @@
 
             <div class="mid-nav">
                 <nav class="navbar navbar-expand-lg">
-                    <ul class="navbar-nav">
+                    <ul class="navbar-nav mx-auto">
                     <li class="nav-item"><a href="/" class="nav-link">Home</a></li>
                         <li class="nav-item"><a href="/" class="nav-link">About Us</a></li>
                         <li class="nav-item"><a href="/" class="nav-link">Destinations</a></li>
@@ -76,7 +78,211 @@
                     </div>
                 </div>
             </div>
+
+            <div class="row">
+                <div class="col-md-12 text-center mt-5 mb-5">
+                    <div class="main-header">
+                        <h6>Come and experience the</h6>
+                        <h3>Big Adventures</h3>
+                        <hr>
+                    </div>
+                </div>
+                <div class=" mb-5 pb-5 col-md-8 mx-auto text-center">
+                    <p class="main-text">Let's take you for an adventure of a lifetime in the heart of Africa. We provide you with an experience like no other! Whether you are a local looking for a new adventure or a visitor to the Red Centre wanting to explore the unknown, we are running flights six days a week for you to come down and enjoy what we have to offer.</p>
+                </div>
+
+            </div>
+            <div class="row mid-banner">
+                <div class="col-md-6 p-0 m-0">
+                    <img src="/storage/frontend/leopard.jpg" alt="" width="100%" height="100%">
+                </div>
+                <div class="col-md-6 p-0 m-0 md-left">
+                <h4>It is better to see something once than to hear about it a thousand times</h4>
+                </div>
+            </div>
+
+            <div class="row " id="services">
+                <div class="col-md-12 text-center mt-5 mb-5">
+                <div class="main-header">
+                        <h6>Come and experience the</h6>
+                        <h3>Our Services</h3>
+                        <hr>
+                    </div>
+                </div> 
+                
+                <div class="col-md-11 mx-auto">
+                <div class="row">
+                    <div class="col-md-4 ">
+                        <div class="card service-col">
+                            <div>
+                                <img src="/storage/frontend/flamingos.jpeg" alt="" width="100%" height="100%">
+                            </div>
+                            <div class="service-text text-center">
+                                <h5>Upclose with flamingos</h5>
+                            </div>
+                            <div class="text-footer">
+                                <h3>Get to meet this adorable birds. Capture the moment</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 ">
+                        <div class="card service-col">
+                            <div>
+                                <img src="/storage/frontend/ocean.jpg" alt="" width="100%" height="100%">
+                            </div>
+                            <div class="service-text text-center">
+                                <h5>Scenic view of the Ocean</h5>
+                            </div>
+                            <div class="text-footer">
+                                <h3>Get a feel of the ocean, enjoying the cool oceanic breeze</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 ">
+                        <div class="card service-col">
+                            <div>
+                                <img src="/storage/frontend/hotels.jpg" alt="" width="100%" height="100%">
+                            </div>
+                            <div class="service-text text-center">
+                                <h5>Luxurious 5-star Hotels</h5>
+                            </div>
+                            <div class="text-footer">
+                                <h3>How about a night in Africa's High-End Restaurants</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            
+            <div class="row" id="explore">
+                <div class="col-md-12 mb-5 text-center mt-5 mb-5">
+                    <div class="main-header">
+                        <h6>Experience of a lifetime</h6>
+                        <h3>Explore the Wild</h3>
+                        <hr>
+                    </div>
+                </div>
+
+                <div class="col-md-12 mx-auto">
+                    <div class="row">
+                    <div class="col-md-6 p-0 m-0">
+                    <div class="img-holder">
+                        <img src="/storage/frontend/nairobipark.jpg" alt="" width="100%" height="100%">
+                    </div>
+                </div>
+                <div class="col-md-6 explore-content p-0 m-0">
+
+                </div>
+
+                <div class="col-md-6 explore-content p-0 m-0">
+                    
+                </div>
+
+                <div class="col-md-6 p-0 m-0">
+                    <div class="img-holder">
+                        <img src="/storage/frontend/maasai.jpg" alt="" width="100%" height="100%">
+                    </div>
+                </div>                
+
+                <div class="col-md-6 p-0 m-0">
+                    <div class="img-holder">
+                        <img src="/storage/frontend/lakenakuru.jpg" alt="" width="100%" height="100%">
+                    </div>
+                </div>
+                <div class="col-md-6 explore-content p-0 m-0">
+                    
+                </div>
+
+
+                <div class="col-md-6 explore-content p-0 m-0">
+                    
+                </div>
+
+                <div class="col-md-6 p-0 m-0">
+                    <div class="img-holder">
+                        <img src="/storage/frontend/elephantnursery.jpg" alt="" width="100%" height="100%">
+                    </div>
+                </div>
+
+                <div class="col-md-6 p-0 m-0">
+                    <div class="img-holder">
+                        <img src="/storage/frontend/amboseli.jpg" alt="" width="100%" height="100%">
+                    </div>
+                </div>
+                <div class="col-md-6 explore-content p-0 m-0">
+                    
+                </div>
+                </div>
+                </div>
+                
+            </div>
+
+            <div class="lower-cta row">
+                <div class="col-md-12 text-center d-flex align-items-center justify-content-center cta-holder">
+                    <p>The World is a Book and those who do not Travel Read only one Page</p>
+                    
+                </div>
+            </div>
+
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.817339529134!2d36.82137331416843!3d-1.2834620359836708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f10d6678064cf%3A0x956bb6f5e0ab2aac!2sMoi%20Ave%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1635619022114!5m2!1sen!2ske" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+        </div>
+    </div>
+
+    <footer>
+        <div class="row">
+            <div class="col-md-3 text-center">
+                <div>
+                    <img src="/storage/logo.png" alt="">
+
+                    <h5>We are a reputable Tours and Travel Company based in Nairobi. We aim to deliver the best services in Kenya and beyond.</h5>
+                </div>
+            </div>
+            <div class="col-md-3">
+                        <h4>Our Services</h4>
+                        <hr class="footer-header">
+                        <ul class="navbar-nav">
+                            <li><i class="mdi mdi-chevron-right"></i>Mt. Kenya NationalPark</li>
+                            <li><i class="mdi mdi-chevron-right"></i>Lake Nakuru</li>
+                            <li><i class="mdi mdi-chevron-right"></i>The Amboseli</li>
+                            <li><i class="mdi mdi-chevron-right"></i>Maasai Lifestyle</li>
+
+                        </ul>
+                    </div>
+
+                    <div class="col-md-3">
+                        <h4>What we promise</h4>
+                        <hr class="footer-header">
+                        <ul class="navbar-nav">
+                            <li><i class="mdi mdi-chevron-right"></i>A Unique Experience</li>
+                            <li><i class="mdi mdi-chevron-right"></i>Pocket-friendly rates</li>
+                            <li><i class="mdi mdi-chevron-right"></i>Customer Friendly experience</li>
+                            <li><i class="mdi mdi-chevron-right"></i>Quality and outstanding Services</li>
+
+                        </ul>
+                    </div>
+
+                    <div class="col-md-3">
+                        <h4>Where to Find us</h4>
+                        <hr class="footer-header">
+                        <ul class="navbar-nav">
+                            <li><i class="mdi mdi-phone-classic"></i>&nbsp;&nbsp;0700112233</li>
+                            <li><i class="fas fa-map-marker-alt "></i>&nbsp;&nbsp;Nairobi, Kenya </li>
+                            <li><i class="mdi mdi-email-open-multiple-outline"></i>&nbsp;&nbsp;info@toursandtravel.co.ke</li>
+                            <li><i class="mdi mdi-email-open-multiple-outline"></i>&nbsp;&nbsp;toursandtravel@gmail.com</li>
+
+                        </ul>
+                    </div>
+        </div>
+
+    </footer>
 </body>
 </html>
